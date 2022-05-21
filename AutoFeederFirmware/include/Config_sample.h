1d699682e9801e38e8fd7dcb414e76d72770b022
blacklizard <jeevanlx@gmail.com>
2022-05-22 04:47:48 +0800

    --wip-- [skip ci]

--- AutoFeederFirmware/include/Config_sample.h
+++ AutoFeederFirmware/include/Config_sample.h
@@ -2,8 +2,9 @@
 #define CONFIG
 
 #define API_ENDPOINT "http://192.168.1.155/api/v1"
-#define WIFI_SSID "Upstair's_WiFi_2.4G"
-#define WIFI_PASSWORD "youcantmesswithme"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
 
 #endif /* CONFIG */
+
 
